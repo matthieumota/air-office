3c312356bf37437d87a0ca670794829c8e8e2ede
Matthieu Mota <matthieumota@gmail.com>
2021-11-29 16:41:23 +0100

    Install Pest

--- tests/Feature/HelloTest.php
+++ tests/Feature/HelloTest.php
@@ -1,17 +1,5 @@
 <?php
 
-namespace Tests\Feature;
-
-use Tests\TestCase;
-
-class HelloTest extends TestCase
-{
-    public function test_user_can_view_homepage()
-    {
-        // On va sur la page d'accueil
-        $response = $this->get('/');
-
-        // On peut vérifier que la page affiche "Laravel"
-        $response->assertSee('Laravel');
-    }
-}
+it('user can view homepage')
+    ->get('/')
+    ->assertSee('Laravel');
