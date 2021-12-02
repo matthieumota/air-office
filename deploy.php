3951a6779ac76e19d0abfcbcd0c96e0d72c8aa13
Matthieu Mota <matthieumota@gmail.com>
2021-12-02 09:46:55 +0100

    Automatic deploy

--- deploy.php
+++ deploy.php
@@ -8,9 +8,17 @@ require 'recipe/laravel.php';
 set('repository', 'git@github.com:matthieumota/air-office.git');
 
 // Hosts
-host('68f8a8b6.nip.io')
+host('prod-68f8a8b6.nip.io')
+    ->set('branch', 'master')
+    ->set('labels', ['stage' => 'prod'])
     ->set('remote_user', 'deployer')
     ->set('deploy_path', '~/air-office');
+
+host('staging-68f8a8b6.nip.io')
+    ->set('branch', 'develop')
+    ->set('labels', ['stage' => 'staging'])
+    ->set('remote_user', 'deployer')
+    ->set('deploy_path', '~/air-office-staging');
 
 // Tasks
 task('build', function () {
