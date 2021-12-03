883eddfdd1c688d853b8dc28ad023a12d700a217
Aymerick Demay <a.demay@lemon-interactive.fr>
2021-12-03 12:12:06 +0100

    Add Moderation Front

--- resources/views/moderation/offices.blade.php
+++ resources/views/moderation/offices.blade.php
@@ -1,9 +1,29 @@
-<h1>Moderation</h1>
+@extends('app')
 
-@if (!empty($offices))
-    <ul>
-        @foreach ($offices as $office)
-        <li>{{ $office->name }}</li> <a href="/moderation/salle-{{ $office->id }}">voir</a>
-        @endforeach
-    </ul>
-@endif
+@section('body')
+    <div class="max-w-4xl mx-auto px-4">
+        <h1 class="text-3xl py-3 mb-3">Bonjour {{ Auth::user()->name }}</h1>
+
+        <a class="py-2 px-4 rounded-md bg-blue-500 text-white" href="/moderation">Retour</a>
+
+        @if (!empty($offices))
+        <div class="bg-white p-8 mt-4 rounded-lg shadow-md">
+            <h2 class="text-3xl py-3 mb-2">Modération des salles</h2>
+
+            <ul class="divide-y divide-gray-300">
+                @foreach ($offices as $office)
+                <li class="py-2 flex items-center justify-between">
+                    <div>
+                        <span class="font-semibold">[{{ $office->user->name }}]</span> - {{ $office->name }}
+                    </div>
+                    <a class="ml-2 py-2 px-4 rounded-md bg-blue-500 text-white" href="/moderation/salle-{{ $office->id }}">Voir</a>
+                </li>
+                @endforeach
+            </ul>
+        </div>
+        @endif
+    </div>
+@endsection
+
+
+
